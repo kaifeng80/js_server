ab52bb9a73c416ddfd30bcaf6fbde69f6d22e8ab
root <root@localhost.localdomain>
2014-06-20 09:18:25 +0800

    add mail.json

--- game-server/app/servers/connector/handler/mailHandler.js
+++ game-server/app/servers/connector/handler/mailHandler.js
@@ -9,7 +9,7 @@ var pomelo = require('pomelo');
 handlerMgr.handler(consts.TYPE_MSG.TYPE_MSG_MAIL, function(msg, session, next) {
     var title = msg.title;
     var content = msg.content;
-    if(0){
+    if(1){
         pomelo.app.get('mail_wrapper').send(title,content);
     }
     else{

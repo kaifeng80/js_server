8930a64bc30b8efff5cb5eeea932a3e7a00f0205
root <root@localhost.localdomain>
2014-06-23 17:41:47 +0800

    update mail.json

--- game-server/app/servers/connector/handler/mailHandler.js
+++ game-server/app/servers/connector/handler/mailHandler.js
@@ -6,12 +6,14 @@ var consts = require("../../../util/consts");
 var mail_wrapper = require('../../../mail/mail_wrapper');
 var redis_mail_wrapper = require('../../../nosql/redis_mail_wrapper');
 var pomelo = require('pomelo');
+var mail_config = require('../../../../config/mail');
+
 handlerMgr.handler(consts.TYPE_MSG.TYPE_MSG_MAIL, function(msg, session, next) {
     var title = msg.title;
     var content = msg.content;
     var channel = msg.channel;
     var version = msg.version;
-    if(0){
+    if(mail_config.send_directly){
         pomelo.app.get('mail_wrapper').send(title,content);
     }
     else{

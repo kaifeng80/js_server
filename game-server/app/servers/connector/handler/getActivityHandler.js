18dd261ad5d2a6a549d914fa351b11bc02579620
King Lee <lijin@chukong-inc.com>
2014-07-15 18:20:42 +0800

    update messgae id

--- game-server/app/servers/connector/handler/getActivityHandler.js
+++ game-server/app/servers/connector/handler/getActivityHandler.js
@@ -4,25 +4,25 @@
 var handlerMgr = require("./../handlerMgr");
 var consts = require("../../../util/consts");
 var pomelo = require('pomelo');
-var activity_templete = require('../../../../config/activity_templete');
 
-handlerMgr.handler(consts.TYPE_MSG.TYPE_SIGN_IN, function(msg, session, next) {
+handlerMgr.handler(consts.TYPE_MSG.TYPE_GET_ACTIVITY, function(msg, session, next) {
     var channel = msg.channel;
     var version = msg.version;
-    var desc = 'default_desc';
+    var type = msg.activity_type;
+    var activity = {};
     var activity_wrapper = pomelo.app.get('activity_wrapper');
     activity_wrapper.get(channel,version,function(activity_json){
         for(var v in activity_json){
-            if(consts.TYPE_ACTIVITY.TYPE_DAILY_SIGN == activity_json[v].type){
-                desc = activity_json[v].desc;
+            if(type == parseInt(activity_json[v].type)){
+                activity = activity_json[v];
             }
         }
         next(null, {
             code: 0,
             msg_id : msg.msg_id,
-            time:Date.now()/1000,
-            type:consts.TYPE_ACTIVITY.TYPE_DAILY_SIGN,
-            desc:desc
+            flowid : msg.flowid,
+            time:Math.floor(Date.now()/1000),
+            activity:activity
         });
     });
 });
--- game-server/app/util/consts.js
+++ game-server/app/util/consts.js
@@ -7,14 +7,12 @@ module.exports = {
         TYPE_MSG_BEGIN :1,
         TYPE_MSG_GET_SRV_TIME:3,
         TYPE_MSG_MAIL:4,
-        TYPE_SIGN_IN:5,
-        TYPE_ACTIVITY_LIST:6,
+        TYPE_GET_ACTIVITY:5,
         TYPE_MSG_END:100
     },
     TYPE_ACTIVITY:{
         TYPE_DAILY_SIGN:1, 	    //// 每日签到
-        TYPE_ACCUMULATE_SIGN:2,	//// 累计签到
-        TYPE_DROP_ITEM:3,		//// 物品掉落
-        TYPE_TASK:4			    //// 每日任务
+        TYPE_DROP_ITEM:2,		//// 物品掉落
+        TYPE_TASK:3			    //// 每日任务
     }
 };

3061aa414d7a0eddf5633f1b7ffc807f4d95cde9
King Lee <lijin@chukong-inc.com>
2014-07-29 13:44:15 +0800

    add 1.2.0 template

--- game-server/app/activity/activity_wrapper.js
+++ game-server/app/activity/activity_wrapper.js
@@ -14,6 +14,7 @@ var activity_wrapper = function() {
 
 activity_wrapper.prototype.init = function(activity_template){
     redis_activity_wrapper.init_activity('template','1.2.4',activity_template);
+    redis_activity_wrapper.init_activity('template','1.2.0',activity_template);
 };
 
 activity_wrapper.prototype.get = function(channel,version,cb){

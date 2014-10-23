aada24122aa8df56a9e82f9e83cc358bfd4d81cd
King Lee <lijin@chukong-inc.com>
2014-10-24 00:05:26 +0800

    update arithmetic week index

--- game-server/app/util/util.js
+++ game-server/app/util/util.js
@@ -6,5 +6,7 @@ var utils = module.exports;
 //  current is the x week
 utils.getWeek = function (date) {
     var onejan = new Date(date.getFullYear(), 0, 1);
-    return Math.ceil((((date - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+    //	delay_day ,such as 3,that means wednesday is the first day of new week
+    var delay_day = 3;
+    return Math.ceil((((date - onejan) / 86400000) + onejan.getDay() + 1 + delay_day) / 7);
 };

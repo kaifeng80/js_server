b8d8b792f610fd88a8d7e0ea5cf00e3267c928d3
King Lee <lijin@chukong-inc.com>
2014-11-07 11:46:52 +0800

    add mac adress for statistics prefix

--- game-server/app/statistics/statistics_wrapper.js
+++ game-server/app/statistics/statistics_wrapper.js
@@ -25,43 +25,50 @@ var statistics_wrapper = function() {
 statistics_wrapper.prototype.tick = function(){
     var self = this;
     setInterval(function(){
-        var date = new Date();
-        var hours = date.getHours();
-        var minutes = date.getMinutes();
-        var work_id = 0;
-        if (cluster.isMaster) {
-            work_id = 0;
-        } else if (cluster.isWorker) {
-            work_id = cluster.worker.id;
-        }
-        if(hours == self.trigger_time_hour && minutes == self.trigger_time_minutes)
-        {
-            //  in all & per day
-            redis_statistics_wrapper.set("requests_in_all" +  ":" + work_id,self.requests_in_all);
-            redis_statistics_wrapper.set("requests_sign_in_all"+  ":" + work_id,self.requests_sign_in_all);
-            redis_statistics_wrapper.set("requests_rank_in_all"+  ":" + work_id,self.requests_rank_in_all);
-            redis_statistics_wrapper.set("requests_rank_in_all_for_running_man"+  ":" + work_id,self.requests_rank_in_all_for_running_man);
+        // Fetch the computer's mac address
+        require('getmac').getMac(function(err,mac_address){
+            if (err)  {
+                console.log(err);
+            }
+            console.log(mac_address);
+            var date = new Date();
+            var hours = date.getHours();
+            var minutes = date.getMinutes();
+            var work_id = 0;
+            if (cluster.isMaster) {
+                work_id = 0;
+            } else if (cluster.isWorker) {
+                work_id = cluster.worker.id;
+            }
+            if(hours == self.trigger_time_hour && minutes == self.trigger_time_minutes)
+            {
+                //  in all & per day
+                redis_statistics_wrapper.set(mac_address + ":" + "requests_in_all" +  ":" + work_id,self.requests_in_all);
+                redis_statistics_wrapper.set(mac_address + ":" + "requests_sign_in_all"+  ":" + work_id,self.requests_sign_in_all);
+                redis_statistics_wrapper.set(mac_address + ":" + "requests_rank_in_all"+  ":" + work_id,self.requests_rank_in_all);
+                redis_statistics_wrapper.set(mac_address + ":" + "requests_rank_in_all_for_running_man"+  ":" + work_id,self.requests_rank_in_all_for_running_man);
 
-            redis_statistics_wrapper.set("requests_per_day"+  ":" + work_id,self.requests_per_day);
-            self.requestsPerDayClear();
+                redis_statistics_wrapper.set(mac_address + ":" + "requests_per_day"+  ":" + work_id,self.requests_per_day);
+                self.requestsPerDayClear();
 
-            redis_statistics_wrapper.set("requests_sign_per_day"+  ":" + work_id,self.requests_sign_per_day);
-            self.requestsSignPerDayClear();
+                redis_statistics_wrapper.set(mac_address + ":" + "requests_sign_per_day"+  ":" + work_id,self.requests_sign_per_day);
+                self.requestsSignPerDayClear();
 
-            redis_statistics_wrapper.set("requests_rank_per_day"+  ":" + work_id,self.requests_rank_per_day);
-            self.requestsRankPerDayClear();
+                redis_statistics_wrapper.set(mac_address + ":" + "requests_rank_per_day"+  ":" + work_id,self.requests_rank_per_day);
+                self.requestsRankPerDayClear();
 
-            redis_statistics_wrapper.set("requests_rank_per_day_for_running_man"+  ":" + work_id,self.requests_rank_per_day_for_running_man);
-            self.requestsRankPerDayClearForRunningMan();
-        }
-        if(minutes == self.trigger_time_minutes)
-        {
-            //  per hour
-            redis_statistics_wrapper.set("requests_per_hour"+  ":" + work_id,self.requests_per_hour);
-            self.requestsPerHourClear();
-        }
-        redis_statistics_wrapper.set("requests_per_minute"+  ":" + work_id,self.requests_per_minute);
-        self.requestsPerMiniuteClear();
+                redis_statistics_wrapper.set(mac_address + ":" + "requests_rank_per_day_for_running_man"+  ":" + work_id,self.requests_rank_per_day_for_running_man);
+                self.requestsRankPerDayClearForRunningMan();
+            }
+            if(minutes == self.trigger_time_minutes)
+            {
+                //  per hour
+                redis_statistics_wrapper.set(mac_address + ":" + "requests_per_hour"+  ":" + work_id,self.requests_per_hour);
+                self.requestsPerHourClear();
+            }
+            redis_statistics_wrapper.set(mac_address + ":" + "requests_per_minute"+  ":" + work_id,self.requests_per_minute);
+            self.requestsPerMiniuteClear();
+        });
     },this.time_interval);
 };
 

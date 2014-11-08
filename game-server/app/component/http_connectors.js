83027504eb311245d7aef1c36bc6f9b043bec2ed
King Lee <lijin@chukong-inc.com>
2014-11-08 16:29:41 +0800

    update deviceid for statistics

--- game-server/app/component/http_connectors.js
+++ game-server/app/component/http_connectors.js
@@ -142,7 +142,7 @@ http_connectors.prototype.dispatchMessage = function(data,url,req,res){
     statistics_wrapper.requestsPerDayInc();
     statistics_wrapper.requestsPerHourInc();
     statistics_wrapper.requestsPerMinuteInc();
-    statistics_wrapper.statistics_device(msg.device_guid);
+    statistics_wrapper.statistics_device(msg.deviceid);
     http_logger.debug("before dispatchMessage ... %j", msg);
     handlerMgr.trigger(msg.msg_id,msg,this.session,function(error,res_msg){
         http_logger.debug("after dispatchMessage ... %j", res_msg);

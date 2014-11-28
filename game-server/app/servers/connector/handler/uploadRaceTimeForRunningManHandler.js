c14c0c9f31672ea066724efee60fc7786b920947
King Lee <lijin@chukong-inc.com>
2014-11-29 02:02:20 +0800

    add tracks for upload rece stores

--- game-server/app/servers/connector/handler/uploadRaceTimeForRunningManHandler.js
+++ game-server/app/servers/connector/handler/uploadRaceTimeForRunningManHandler.js
@@ -52,6 +52,44 @@ handlerMgr.handler(consts.TYPE_MSG.TYPE_UPLOAD_RACE_TIME_FOR_RUNNING_MAN, functi
                 if("true" == is_increase_level){
                     rank_running_man_wrapper.increase_level(championship_id,device_guid,increase_level_num);
                 }
+                //  calc award tracks
+                var award_tracks_number_at_last = 0;
+                var award_tracks_days = activity.award_tracks_days ? activity.award_tracks_days: [6,0];
+                var award_tracks_hours = activity.award_tracks_hours ? activity.award_tracks_hours: [0,23];
+                var award_tracks_ranks = activity.award_tracks_ranks ? activity.award_tracks_ranks: [1,2,3];
+                var award_tracks_numbers = activity.award_tracks_numbers ? activity.award_tracks_numbers: 1;
+                var cur_date = new Date();
+                var cur_day = cur_date.getDay();
+                var cur_hour = cur_date.getHours();
+                var is_award_tracks_days = false;
+                for(var i = 0; i < award_tracks_days.length; ++i){
+                    if(award_tracks_days[i] == cur_day){
+                        is_award_tracks_days = true;
+                        break;
+                    }
+                }
+                //  step 1: is the award tracks day
+                if(is_award_tracks_days){
+                    //  step 2: is the award tracks hours
+                    var is_award_tracks_hours = false;
+                    if(cur_hour >= award_tracks_hours[0] && cur_hour <= award_tracks_hours[1]){
+                        is_award_tracks_hours = true;
+                    }
+                    if(is_award_tracks_hours){
+                        //  step 3: is the award tracks rank
+                        var is_award_tracks_ranks = false;
+                        for(var k = 0; k < award_tracks_ranks.length; ++k){
+                            if(mine_rank == award_tracks_ranks[k]){
+                                is_award_tracks_ranks = true;
+                                break;
+                            }
+                        }
+                        if(is_award_tracks_ranks){
+                            //  step 4: calc award tracks number
+                            award_tracks_number_at_last = award_tracks_numbers;
+                        }
+                    }
+                }
                 rank_running_man_wrapper.add_rank_info(championship_id,device_guid,is_add_score,finally_score,rank_info,function(result){
                     rank_running_man_wrapper.get_rank(championship_id,device_guid,function(reply){
                         next(null, {
@@ -62,6 +100,8 @@ handlerMgr.handler(consts.TYPE_MSG.TYPE_UPLOAD_RACE_TIME_FOR_RUNNING_MAN, functi
                             rank:reply[0] != null ? parseInt(reply[0]) + 1: reply[0],
                             score:reply[1] != null ? parseInt(reply[1]): reply[1],
                             player_number:reply[3] != null ? reply[3]: 0,
+                            tracks:award_tracks_number_at_last,
+                            is_add_score:is_add_score,
                             time: Math.floor(Date.now() / 1000)
                         });
                     });

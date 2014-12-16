b9b4f2a48eafabc2f456ecf8773d8fa22348b64b
King Lee <lijin@chukong-inc.com>
2014-12-16 17:58:03 +0800

    update random_prize json file

--- game-server/app/random_prize/random_prize_the_second_phase_wrapper.js
+++ game-server/app/random_prize/random_prize_the_second_phase_wrapper.js
@@ -2,26 +2,18 @@
  * Created by King Lee on 2014/9/16.
  */
 var redis_random_prize_the_second_phase_wrapper = require('../nosql/redis_random_prize_the_second_phase_wrapper');
-var gacha_json = require('../../config/gacha.json');
-var gacha_json2 = require('../../config/gacha2.json');
+var gacha_the_second_phase_json = require('../../config/gacha_the_second_phase.json');
 var random_prize_the_second_phase_wrapper = function() {
     this.wight_total = 0;
-    this.wight_total2 = 0;
     this.wight_array = [];
-    this.wight_array2 = [];
     this.init();
 };
 
 random_prize_the_second_phase_wrapper.prototype.init = function(){
-    for(var i = 0; i < gacha_json.length; ++i){
+    for(var i = 0; i < gacha_the_second_phase_json.length; ++i){
         var wight_total_backup = this.wight_total;
-        this.wight_total += gacha_json[i].rate;
-        this.wight_array.push({"id":gacha_json[i].id,"range":[wight_total_backup,this.wight_total]});
-    }
-    for(var i = 0; i < gacha_json2.length; ++i){
-        var wight_total_backup2 = this.wight_total2;
-        this.wight_total2 += gacha_json2[i].rate;
-        this.wight_array2.push({"id":gacha_json2[i].id,"range":[wight_total_backup2,this.wight_total2]});
+        this.wight_total += gacha_the_second_phase_json[i].rate;
+        this.wight_array.push({"id":gacha_the_second_phase_json[i].id,"range":[wight_total_backup,this.wight_total]});
     }
 };
 
@@ -34,26 +26,9 @@ random_prize_the_second_phase_wrapper.prototype.random = function(){
             break;
         }
     }
-    for(i = 0; i < gacha_json.length; ++i){
+    for(i = 0; i < gacha_the_second_phase_json.length; ++i){
         if(i == index){
-            return gacha_json[i];
-        }
-    }
-    return null;
-};
-
-random_prize_the_second_phase_wrapper.prototype.random2 = function(){
-    var random_value = Math.floor(Math.random()*this.wight_total2);
-    var index = 0;
-    for(var i = 0; i < this.wight_array2.length; ++i){
-        if(random_value >= this.wight_array2[i].range[0] && random_value < this.wight_array2[i].range[1]){
-            index = i;
-            break;
-        }
-    }
-    for(i = 0; i < gacha_json2.length; ++i){
-        if(i == index){
-            return gacha_json2[i];
+            return gacha_the_second_phase_json[i];
         }
     }
     return null;

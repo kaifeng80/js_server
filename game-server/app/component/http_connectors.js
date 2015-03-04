17732f0049762a5d49e0e829c8504fe9c8fe60d1
King Lee <lijin@chukong-inc.com>
2015-03-04 10:42:37 +0800

    add version 1.0.1

--- game-server/app/component/http_connectors.js
+++ game-server/app/component/http_connectors.js
@@ -150,7 +150,7 @@ http_connectors.prototype.dispatchMessage = function(data,url,req,res){
     if(msg.version){
         if("1.2.9" == msg.version){
             msg.version = "1.2.8";
-        }else if("1.0.0" == msg.version || "0.9.0" == msg.version){
+        }else if("1.0.0" == msg.version || "0.9.0" == msg.version || "1.0.1" == msg.version){
             //  version mapping for wandoujia
             msg.version = "2.2.0";
         }

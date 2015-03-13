9e583d0ca98266ff859bb92ad576d9d3a938df12
King Lee <lijin@chukong-inc.com>
2015-03-13 15:45:30 +0800

    fix bug for grade error

--- game-server/app/servers/connector/handler/getRankPartialForPVP.js
+++ game-server/app/servers/connector/handler/getRankPartialForPVP.js
@@ -163,7 +163,7 @@ handlerMgr.handler(consts.TYPE_MSG.TYPE_GET_RANK_PARTIAL_FOR_PVP, function (msg,
                         var rank_info = JSON.parse(rank_info_array_weekly[i]);
                         if (rank_info) {
                             for (var v in rival_vs_title_json) {
-                                if (rival_vs_title_json[v].score <= rank_info.score_weekly) {
+                                if (rival_vs_title_json[v].score <= rank_info.score) {
                                     degree_title = rival_vs_title_json[v].title;
                                     degree = rival_vs_title_json[v].grade;
                                 }
@@ -185,7 +185,7 @@ handlerMgr.handler(consts.TYPE_MSG.TYPE_GET_RANK_PARTIAL_FOR_PVP, function (msg,
                         var rank_info = JSON.parse(rank_info_array_activity[i]);
                         if (rank_info) {
                             for (var v in rival_vs_title_json) {
-                                if (rival_vs_title_json[v].score <= rank_info.score_weekly) {
+                                if (rival_vs_title_json[v].score <= rank_info.score) {
                                     degree_title = rival_vs_title_json[v].title;
                                     degree = rival_vs_title_json[v].grade;
                                 }
